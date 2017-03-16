9e4504dcad401d38f20e23a2d74cb539f13029cf
David Gunter <david.gunter@tivix.com>
2017-03-15 18:14:07 -0700

    Add exceptions from API, add response template for get_random_pokemon

--- randompokemon.py
+++ randompokemon.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 ask = Ask(app, "/")
 
 REGIONS = ['Kanto', 'Johto', 'Hoenn', 'Sinnoh', 'Unova', 'Kalos', 'Alola']
-POKEMON_TYPES = []
 
 @ask.launch
 def launch():
@@ -42,27 +41,35 @@ def oneshot_pokemon():
 
 @ask.intent("GetSpecificPokemon",
             mapping={'number_of_pokemon': 'Count', 'pokemon_type': 'Type', 'region': 'Region'},
-            convert={'count': int},
+            convert={'number_of_pokemon': int},
             default={'number_of_pokemon': 1, 'pokemon_type': 'any', 'region': 'national'})
 def get_random_pokemon(number_of_pokemon, pokemon_type, region):
     """
     Uses user-provided slots to request random pokemon that meet constraints from slots
     """
     if not 1 <= number_of_pokemon <= 6:
-        return question(render_template("number_of_pokemon_error").reprompt("generate_reprompt"))
-
-    if region not in REGIONS:
-        return question(render_template("region_error").reprompt("generate_reprompt"))
-
-    if pokemon_type not in POKEMON_TYPES:
-        return question(render_template("pokemon_type_error").reprompt("generate_reprompt"))
+        return question(render_template("number_of_pokemon_error").reprompt("generate_reprompt"))        
 
     try:
         api_response = _send_api_request(number_of_pokemon, pokemon_type, region)
+
+    except InvalidPokemonType:
+        return question(render_template("pokemon_type_error").reprompt("generate_reprompt"))
+
+    except InvalidPokemonRegion:
+        return question(render_template("region_error").reprompt("generate_reprompt"))
+
     except:
         return statement(render_template("genator_problem")).simple_card("Random Pokemon Generator", 
                                                                         render_template("genator_problem"))
-    pass
+    # Format results so they are correctly read by Alexa
+    generated_pokemon = ", ".join(_format_pokemon_as_list(api_response))
+    statement_text = render_template("custom_generated_pokemon", 
+                                     num_pokemon=number_of_pokemon,
+                                     pokemon_type=pokemon_type,
+                                     region=region,
+                                     pokemon=generated_pokemon)
+    return statement(statement_text)
 
 @ask.intent("ListOfRegions")
 def get_list_of_regions():
@@ -84,7 +91,11 @@ def _send_api_request(num_pokemon, pokemon_type="any", region="national"):
     response = urlopen(ENDPOINT + "/generate", data=data)
 
     if response.code != 200:
-        raise Exception("Response returned an error")
+        errors = response.json()
+        if errors.get('type1') or errors.get('type2'):
+            raise InvalidPokemonType
+        else:
+            raise Exception("Response returned an error")
     return response.read()
 
 
@@ -101,6 +112,16 @@ def _format_pokemon_as_list(response):
         list_of_pokemon.insert(-1, 'and')
 
     return list_of_pokemon
+
+
+class InvalidPokemonType(Exception):
+    """Raise if Pokemon Type given is not one of the real Pokemon types"""
+    pass
+
+
+class InvalidPokemonRegion(Exception):
+    """Raise if Pokemon Region is not valid"""
+    pass
 
 
 if __name__ == '__main__':
